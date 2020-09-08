05ad177cecc7a1555b2cd7a6ff313c42e8215085
soarflat <soarflat777@gmail.com>
2020-09-08 21:39:11 +0900

    chore: Update babel.config.js

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,14 @@
 module.exports = {
-  presets: [['@babel/preset-env', { targets: { node: 'current' } }]]
+  env: {
+    test: {
+      presets: [
+        [
+          '@babel/preset-env',
+          {
+            targets: { node: 'current' },
+          },
+        ],
+      ],
+    },
+  },
 };
